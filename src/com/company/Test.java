d1d20ca48787eac366c825a2d6ba927d1354093c
Roman Kozhevnikow <roman.kozhevnikow@gmail.com>
2021-08-05 13:00:06 +0300

    First commit

---
+++ src/com/company/Test.java
@@ -0,0 +1,11 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Locale;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+}
+}
